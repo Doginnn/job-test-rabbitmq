b5191669b693ce10d1ef6e47d2020b7e219a5d39
doginnn <diogenesemmanuel@gmail.com>
2022-07-03 18:48:04 -0300

    [ADD] Configuring producer action annotation route.

--- producer-teste/src/Controller/SendAction.php
+++ producer-teste/src/Controller/SendAction.php
@@ -6,9 +6,16 @@ use OldSound\RabbitMqBundle\RabbitMq\ProducerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class SendAction
 {
+    /**
+     * @param Request $request
+     * @param ProducerInterface $sendProducer
+     * @return JsonResponse
+     * @Route(name="send_post", methods={"POST"}, path="/send")
+     */
     public function __invoke(Request $request, ProducerInterface $sendProducer)
     {
         $content = $request->getContent();
